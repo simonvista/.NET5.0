f8eee9c4900cffdb1e7a41fbb7def25510afc2f0
YL <simon-ling@hotmail.com>
2021-08-01 22:41:10 -0400

    11040 is done

--- Controllers/AppointmentController.cs
+++ Controllers/AppointmentController.cs
@@ -8,12 +8,19 @@ namespace InAndOut.Controllers
 {
     public class AppointmentController : Controller
     {
+        // https://localhost:44319/appointment
         public IActionResult Index()
         {
             return View();
             //string todaysDate = DateTime.Now.ToShortDateString();
-            //return Ok(todaysDate);
+            //return Ok(todaysDate);          
 
         }
+        // https://localhost:44319/appointment/details/id
+        public IActionResult Details(int id)
+        {
+            return Ok($"You've entered id: {id}");
+        }
     }
+
 }
