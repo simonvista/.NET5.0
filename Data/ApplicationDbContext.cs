cc9c2b3da6b832b6c0b622be3a8f7f6a30016378
YL <simon-ling@hotmail.com>
2021-08-02 20:21:58 -0400

    20454 is done

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace InAndOut.Data
         {
 
         }
+        // Items will be the table name in MS-SQL server DB
+        public DbSet<Item> Items { get; set; }
     }
 }
